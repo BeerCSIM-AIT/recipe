a1d8495b42349b5482c0cb9a2e86fc3a9e716e32
Phakpoom Ittirattanakomon <phakpoomittirattanakomon@Phakpooms-MacBook-Pro.local>
2022-08-28 11:17:33 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Food Recipes",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
